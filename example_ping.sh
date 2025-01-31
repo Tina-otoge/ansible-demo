7090dfc70240c6a0c45246cc423f4fd249671ef2
Damien Savatier <damien.savatier@visian.fr>
2025-01-31 05:10:12 +0100

    Add more raw commands examples

---
+++ example_ping.sh
@@ -0,0 +1,1 @@
+ansible my-server -m ping
---
+++ example_run_playbooks.sh
@@ -0,0 +1,8 @@
+# Replace basic scripting with a playbook
+ansible-playbook playbooks/example_hostname.yml
+
+# Some standard security recommendations implemented as a playbook
+ansible-playbook playbooks/example_security.yml
+
+# A playbook to deploy a basic web application
+ansible-playbook playbooks/example_application.yml
